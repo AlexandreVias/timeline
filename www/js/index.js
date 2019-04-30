3fc25f51c3262ecb5152e6589eecbbb812f0a4aa
Alexandre <alexandre.vias@hotmail.fr>
2019-04-30 14:18:49 +0200

    init

---
+++ www/js/index.js
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function () {
+        this.receivedEvent('deviceready');
+        document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture);
+        localStorage.setItem('articles', JSON.stringify([
+            {
+                title: 'Trop belle photo',
+                description: 'Une petite description.',
+                date: 'Today',
+                img: 'img/pexels-photo.jpg'
+            },
+            {
+                title: 'Je sais pas quoi mettre comme titre',
+                description: 'Une autre description.',
+                date: 'Yesterday',
+                img: 'img/beauty-bloom-blue-67636.jpg'
+            }
+        ]));
+        loadArticles();
+    },
+
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+function cameraTakePicture() {
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
+function loadArticles() {
+    let articles = JSON.parse(localStorage.getItem('articles'));
+    for (let i = 0; i < articles.length; i++) {
+        let html = `<div id="${i}">`;
+        html += `<div><h4>${articles[i].title}</h4></div>`;
+        html += `<div><img src="${articles[i].img}" class="imgArticle" alt="Image of article"></div>`;
+        html += `</div>`;
+        document.getElementById('articles').innerHTML += html;
+        addEvent(i);
+    }
+}
+
+function addEvent(id) {
+    document.getElementById(id).onclick = () => {
+        alert('Article n° ' + id);
+    }
+}
