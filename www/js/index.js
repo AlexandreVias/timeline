557f6b26658cda81dd13671e6259d51e91439434
Alexandre <alexandre.vias@hotmail.fr>
2019-05-19 18:55:08 +0200

    Add video

--- www/js/index.js
+++ www/js/index.js
@@ -25,6 +25,7 @@ let app = {
     onDeviceReady: function () {
         this.receivedEvent('deviceready');
         document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture);
+        document.getElementById("cameraTakeVideo").addEventListener("click", cameraTakeVideo);
         /*localStorage.setItem('articles', JSON.stringify([
             {
                 title: 'Trop belle photo',
@@ -69,8 +70,22 @@ function cameraTakePicture() {
     });
 
     function onSuccess(imageData) {
-        document.getElementById('photoDiv').style.display = 'inline-block';
+        document.getElementById('photoDiv').style.display = 'block';
         document.getElementById('photo').src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
+function cameraTakeVideo() {
+    navigator.device.capture.captureVideo(onSuccess, onFail);
+
+    function onSuccess(files) {
+        document.getElementById('videoSource').type = files[0].type;
+        document.getElementById('video').src = files[0].fullPath;
+        document.getElementById('videoDiv').style.display = 'block';
     }
 
     function onFail(message) {
@@ -84,11 +99,14 @@ function loadArticles() {
         let art = articles[i];
         let html = `<div id="${i}">`;
         html += `<div><h4>${art.title}</h4></div>`;
-        html += `<div><img src="${art.img}" class="imgArticle" alt="Image of article"></div>`;
+        if (art.hasOwnProperty('img'))
+            html += `<div><img src="${art.img}" class="imgArticle" alt="Image of article"></div>`;
         html += `<span class="toggle">></span>`;
         html += `<div class="details">`;
         html += `<p>Description: ${art.description}</p>`;
         html += `<p>Date: ${art.date}</p>`;
+        if (art.hasOwnProperty('video'))
+            html += `<video controls width="100%" src="${art.video.src}"><source type="${art.video.type}"></video>`;
         html += `</div></div>`;
         document.getElementById('articles').innerHTML += html;
     }
@@ -105,16 +123,41 @@ function addEvent(lenght) {
 }
 
 document.getElementById('add').onclick = () => addArticleJSON();
+document.getElementById('reset').onclick = () => reset();
 
 function addArticleJSON() {
+    const title = document.getElementById('title').value;
+    const date = document.getElementById('date').value;
+    if (title === '') {
+        alert('Title required');
+        return;
+    }
+    if (date === '') {
+        alert('Date required');
+        return;
+    }
     let articles = JSON.parse(localStorage.getItem('articles'));
-    articles.push({
-        title: document.getElementById('title').value,
+    const hasPhoto = document.getElementById('photoDiv').style.display === 'block';
+    const hasVideo = document.getElementById('videoDiv').style.display === 'block';
+    let art = {
+        title: title,
         description: document.getElementById('description').value,
-        date: document.getElementById('date').value,
-        img: document.getElementById('photo').src
-    });
+        date: date
+    };
+    if (hasPhoto)
+        art.img = document.getElementById('photo').src;
+    if (hasVideo)
+        art.video = {
+            src: document.getElementById('video').src,
+            type: document.getElementById('videoSource').type
+        };
+    articles.push(art);
     localStorage.setItem('articles', JSON.stringify(articles));
+    location.reload()
+}
+
+function reset() {
+    localStorage.setItem('articles', JSON.stringify([]));
     location.reload()
 }
 
@@ -125,23 +168,22 @@ var button = document.querySelector('#hamburger-button');
 var overlay = document.querySelector('#hamburger-overlay');
 var activatedClass = 'hamburger-activated';
 
-button.addEventListener('click', function(e) {
+button.addEventListener('click', function (e) {
     e.preventDefault();
 
     this.parentNode.classList.add(activatedClass);
 });
 
 
-button.addEventListener('keydown', function(e) {
-    if (this.parentNode.classList.contains(activatedClass))
-    {
+button.addEventListener('keydown', function (e) {
+    if (this.parentNode.classList.contains(activatedClass)) {
         if (e.repeat === false && e.which === 27)
             this.parentNode.classList.remove(activatedClass);
     }
 });
 
 
-overlay.addEventListener('click', function(e) {
+overlay.addEventListener('click', function (e) {
     e.preventDefault();
 
     this.parentNode.classList.remove(activatedClass);
